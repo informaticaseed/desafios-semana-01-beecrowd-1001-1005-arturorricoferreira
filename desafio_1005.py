67018606001820473c92fb42e7c20c10af66ce36
arturorricoferreira <arturorricoferreira@gmail.com>
2026-02-27 09:10:42 -0300

    Update desafio_1005.py

--- desafio_1005.py
+++ desafio_1005.py
@@ -18,14 +18,13 @@ de imprimir o fim de linha após o resultado, caso contrário, você receberá
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1005
 
 # Escreva sua solução abaixo
-#include <stdio.h>
+#include <stdio.h
+# Leitura das notas
+A = float(input())
+B = float(input())
 
-int main() {double A, B, MEDIA;
-    scanf("%lf", &A);
-    scanf("%lf", &B);
-    
-    MEDIA = (A * 3.5 + B * 7.5) / 11.0;
-    
-    printf("MEDIA = %.1f\n", MEDIA);
-    
-    return 0;}
+# Cálculo da média ponderada
+media = (A * 3.5 + B * 7.5) / 11
+
+# Saída formatada
+print(f"MEDIA = {media:.1f}")
